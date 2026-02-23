0c9300b1106abe0f1a9eb9f153d35885eff0d268
JoelMoirangthem <joelmoirang@gmail.com>
2026-02-23 21:57:34 +0530

    Initiallise Seir project

---
+++ lab1.py
@@ -0,0 +1,40 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+def extractWebpage(websiteLink):
+    header = {
+        'User-Agent' : 'Mozilla'
+    }
+    outputData = requests.get(websiteLink,headers = header)
+
+    return outputData.text
+
+def webPageData(webpage):
+
+    soup = BeautifulSoup(webpage,"html.parser") 
+    
+    title = ""
+    if(soup.title):
+        title = soup.title.string
+        
+
+    
+    body = soup.body.get_text(" ", strip=True)
+    
+
+    links = [] 
+   
+    for anchor_tag in soup.find_all("a"):
+        link = anchor_tag.get("href")
+        if link:
+            links.append(link)
+    return title , body , links
+
+url = sys.argv[1]
+html = extractWebpage(url)
+title, body, links = webPageData(html)
+print(title)
+print(body)
+for link in links:
+    print(link)
