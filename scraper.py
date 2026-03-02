0a439b29895c063731caca9418fee3e7cc5bd7a5
JoelMoirangthem <joelmoirang@gmail.com>
2026-03-02 14:40:07 +0530

    Changing file name

--- scraper.py
+++ scraper.py
@@ -2,39 +2,49 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+
 def extractWebpage(websiteLink):
     header = {
-        'User-Agent' : 'Mozilla'
+        'User-Agent': 'Mozilla'
     }
-    outputData = requests.get(websiteLink,headers = header)
 
+   
+    if not websiteLink.startswith("http://") and not websiteLink.startswith("https://"):
+        websiteLink = "https://" + websiteLink
+
+    outputData = requests.get(websiteLink, headers=header)
     return outputData.text
+
 
 def webPageData(webpage):
 
-    soup = BeautifulSoup(webpage,"html.parser") 
-    
+    soup = BeautifulSoup(webpage, "html.parser")
+
     title = ""
-    if(soup.title):
+    if soup.title:
         title = soup.title.string
-        
 
-    
-    body = soup.body.get_text(" ", strip=True)
-    
+    body = ""
+    if soup.body:
+        body = soup.body.get_text(" ", strip=True)
 
-    links = [] 
-   
+    links = []
+
     for anchor_tag in soup.find_all("a"):
         link = anchor_tag.get("href")
         if link:
             links.append(link)
-    return title , body , links
+
+    return title, body, links
+
 
 url = sys.argv[1]
+
 html = extractWebpage(url)
 title, body, links = webPageData(html)
+
 print(title)
 print(body)
+
 for link in links:
     print(link)
